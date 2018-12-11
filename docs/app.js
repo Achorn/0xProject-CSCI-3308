a393f32c03eb6efc2690587b671789482c667488
thenamesmooch <42759313+thenamesmooch@users.noreply.github.com>
2018-12-11 11:06:45 -0700

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -36,20 +36,6 @@
 
   addIngredientButton.addEventListener('click', e => {
     firebase.database().ref('users/' + firebase.auth().currentUser.uid + "/Ingredients").push(txtIngredient.value);
-    var recipeRef = firebase.database().ref('recipes/');
-    database = firebase.database();
-
-    var ref = database.ref('recipes');
-    ref.on('value', gotData, errData);
-    function gotData(data) {
-      console.log(data.val());
-    }
-    function errData(err) {
-      console.log('Error!');
-      console.log(err);
-
-    }
-
   });
 
   document.getElementById("ingredient-body").addEventListener("click", function(e){

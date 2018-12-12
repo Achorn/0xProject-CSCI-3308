bb42f9068666cbce7567a7794b4b06ca88e2b70a
Joshua Achorn <Joshuaachorn@gmail.com>
2018-12-12 10:08:39 -0700

    no longer need to reload page to se recipes

--- docs/app.js
+++ docs/app.js
@@ -15,6 +15,7 @@
   var ingredientDisplay = firebase.database().ref().child("users");
   var recipeDisplay = firebase.database().ref().child("recipes");
   var userID = ""
+  var includedRecipes = [];
 
   //logIn
   const txtEmail = document.getElementById('email');
@@ -57,11 +58,60 @@
       });
     }, e => alert(e.message));
   });
+
   logOutButton.addEventListener('click', e => {
     const promise = firebase.auth().signOut();
     promise.catch(e => alert(e.message));
   });
 
+  function displayrecipes(snap){
+    console.log('recipe function called');
+    $("#recipe-body").empty();
+    recipeDisplay.on("child_added", snap=>{
+      console.log("in recipeDisplay child added")
+      includedRecipes = [];
+      var website = (snap.val()[0]);
+      var userIngredientsArray = [];
+      var userIngredient = "";
+      ingredientDisplay.child(userID + "/Ingredients").on("child_added", snap => {
+        var userIngredient = snap.val();
+        userIngredientsArray.push(userIngredient);
+      });
+      //for each recipe in database, add to array then filter out each recipe
+      //that doesnt belong one at a time
+      var recipeArray = (snap.val());
+      includedRecipes.push([snap.key,website]);
+      recipeArray.shift(); //remove website
+
+      //loop for every ingredient a user inputs
+      for(var i=0; i < userIngredientsArray.length; i++){
+        userIngredient = userIngredientsArray[i];
+        //console.log(userIngredient);
+        var found = false;
+
+        //loop for searching users ingredient with recipe ingredients
+        recipeArray.forEach(function(recIngredient, recIndex){
+          //console.log(RecIngrrdient);
+          var check = recIngredient.search(userIngredient);
+          if (check != -1) {
+            // console.log("user ingredient", userIngredient);
+            // console.log("recipe ingredient", recIngredient);
+            found = true;
+          }
+        });
+        //if ingredient was not found
+        if(found === false){
+          includedRecipes.pop();
+          break;
+        }
+      }
+      if(found === true){
+        var recLink = includedRecipes[includedRecipes.length - 1][0].link(includedRecipes[includedRecipes.length - 1][1]);
+
+      $("#recipe-body").append("<tr><td>"+ recLink +"</td></tr>");
+      }
+    });
+  }
 
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
@@ -73,61 +123,14 @@
       ingredientDisplay.child(userID + "/Ingredients").on("child_added", snap => {
         var ingredient = snap.val();
         $("#ingredient-body").append("<tr><td>"+ ingredient + "</td><td><button class='deleteIngredientButton' id='" + snap.key + "'>X</button></td></tr>");
+        displayrecipes();
       });
       ingredientDisplay.child(userID + "/Ingredients").on("child_removed", snap => {
         //console.log(snap.key);
         $("#" + snap.key).closest('tr').remove();
+        displayrecipes();
       });
-
-
-
-      var includedRecipes = [];
-      recipeDisplay.on("child_added", snap =>{
-
-        var website = (snap.val()[0]);
-
-        //add users ingredients to array
-        var userIngredientsArray = [];
-        ingredientDisplay.child(userID + "/Ingredients").on("child_added", snap => {
-          var userIngredient = snap.val();
-          userIngredientsArray.push(userIngredient);
-        });
-
-        //for each recipe in database, add to array then filter out each recipe
-        //that doesnt belong one at a time
-        var recipeArray = (snap.val());
-        includedRecipes.push([snap.key,website]);
-        recipeArray.shift(); //remove website
-
-        //loop for every ingredient a user inputs
-        for(var i=0; i < userIngredientsArray.length; i++){
-          userIngredient = userIngredientsArray[i];
-          //console.log(userIngredient);
-          var found = false;
-
-          //loop for searching users ingredient with recipe ingredients
-          recipeArray.forEach(function(recIngredient, recIndex){
-            //console.log(RecIngrrdient);
-            var check = recIngredient.search(userIngredient);
-            if (check != -1) {
-              // console.log("user ingredient", userIngredient);
-              // console.log("recipe ingredient", recIngredient);
-              found = true;
-            }
-          });
-          //if ingredient was not found
-          if(found === false){
-            includedRecipes.pop();
-            break;
-          }
-        }
-        if(found === true){
-          var recLink = includedRecipes[includedRecipes.length - 1][0].link(includedRecipes[includedRecipes.length - 1][1]);
-
-        $("#recipe-body").append("<tr><td>"+ recLink +"</td></tr>");
-        }
-      });
-      console.log("Included recipes", includedRecipes);
+      // displayrecipes();
       // ...
     } else {
       // User is signed out.
